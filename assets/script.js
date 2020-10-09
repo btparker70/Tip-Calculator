34a441eba3aaa8068e92a1091cf3425f11042045
Brian Parker <btparker70@gmail.com>
2020-10-09 16:11:20 -0700

    js added. needs debugging

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,42 @@
+//Calculator
 function calculatorTip() {
     var billTotal = document.getElementById("billTotal").nodeValue;
     var serviceQuality = document.getElementById("serviceQuality").nodeValue;
+    var totalPeople = document.getElementById("peopleTotal").nodeValue;
+
+    // Validate Input
+    if (billTotal === "" || serviceQuality == 0) {
+        alert("Please enter values");
+        return;
+    }
+
+    //Check to see if input is empty or <=1
+    if (totalPeople === "" || totalPeople <= 1) {
+        totalPeople = 1;
+
+        document.getElementById("each").style.display = "none";
+    } else {
+        document.getElementById("each").style.display = "block";
+    }
+
+    //Calculate Tip
+    var total = (billTotal * serviceQuality) / totalPeople;
+    
+    //Round to 2 decimal places
+    total = Math.round(total * 100) / 100;
+
+    //Always give 2 decimal places
+    total = total.toFixed(2);
+
+    //Display Tip
+    document.getElementById("tip").innerHTML = total;
 }
+
+//Hide tip amount on load
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("each").style.display = "none";
+
+//Click to call function
+document.getElementById("calculate").onclick = function() {
+    calculateTip();
+};
